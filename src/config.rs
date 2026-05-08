370a51ba721ff3d8ac3b744bd29758f547068de3
JulesEfrei <jules.bruzeau@efrei.net>
2026-05-08 12:22:07 +0200

    fix: improve error handling and add fallback to user config dir

--- src/config.rs
+++ src/config.rs
@@ -1,30 +1,33 @@
-use crate::logger::log;
 use core::fmt;
-use directories::{ProjectDirs, UserDirs};
+use directories::{BaseDirs, ProjectDirs, UserDirs};
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
-pub struct ConfigError {
-    message: String,
-}
-
-impl ConfigError {
-    fn new(message: impl Into<String>) -> Self {
-        Self {
-            message: message.into(),
-        }
-    }
+pub enum ConfigError {
+    FileNotFound,
+    DirectoryNotFound,
+    WriteError(String),
+    CreationError(String),
+    ParseError(String),
 }
 
 impl fmt::Display for ConfigError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "Error while loading the configuration:\n{}",
-            self.message
-        )
+        match self {
+            ConfigError::FileNotFound => write!(f, "Configuration file not found."),
+            ConfigError::DirectoryNotFound => write!(f, "Directory not found."),
+            ConfigError::WriteError(message) => {
+                write!(f, "Error while writting into the file: {message}")
+            }
+            ConfigError::CreationError(message) => {
+                write!(f, "Error while creating file or directories: {message}")
+            }
+            ConfigError::ParseError(message) => {
+                write!(f, "Error while parsing the configuration: {message}")
+            }
+        }
     }
 }
 
@@ -41,28 +44,14 @@ impl AppConfig {
         let config_file = Self::get_config_file()?;
 
         if config_file.exists() {
-            log!(
-                "INFO",
-                format!("The config file exists at {}", config_file.display())
-            );
+            let config =
+                fs::read_to_string(&config_file).map_err(|_e| ConfigError::FileNotFound)?;
 
-            let config = fs::read_to_string(&config_file).map_err(|e| {
-                ConfigError::new(format!(
-                    "Unable to read config file {}: {e}",
-                    config_file.display()
-                ))
-            })?;
-
-            let parsed = serde_yml::from_str::<AppConfig>(&config).map_err(|e| {
-                ConfigError::new(format!(
-                    "Unable to parse config file {}: {e}",
-                    config_file.display()
-                ))
-            })?;
+            let parsed = serde_yml::from_str::<AppConfig>(&config)
+                .map_err(|e| ConfigError::ParseError(e.to_string()))?;
 
             Ok(parsed)
         } else {
-            log!("INFO", "The config file doesn't exist. Creating...");
             Self::setup_default_config(&config_file)
         }
     }
@@ -71,7 +60,7 @@ impl AppConfig {
         let project_directory = ProjectDirs::from("com", "bayne", "qn");
 
         match project_directory {
-            None => Err(ConfigError::new("Unable to get the project directory")),
+            None => Err(ConfigError::DirectoryNotFound),
             Some(project_directory) => Ok(project_directory.config_dir().to_path_buf()),
         }
     }
@@ -81,25 +70,35 @@ impl AppConfig {
     }
 
     fn get_document_directory() -> Result<PathBuf, ConfigError> {
-        let user_dir =
-            UserDirs::new().ok_or_else(|| ConfigError::new("Unable to get the user directory"))?;
+        let user_dir = UserDirs::new();
 
-        let document_dir = user_dir
-            .document_dir()
-            .ok_or_else(|| ConfigError::new("Unable to get document directory"))?;
+        match user_dir {
+            Some(user_dir) => {
+                let document_dir = user_dir
+                    .document_dir()
+                    .ok_or(ConfigError::DirectoryNotFound)?;
 
-        Ok(document_dir.to_path_buf())
+                Ok(document_dir.to_path_buf())
+            }
+            None => {
+                let base_dir = BaseDirs::new().ok_or(ConfigError::DirectoryNotFound)?;
+                Ok(base_dir.home_dir().to_path_buf())
+            }
+        }
     }
 
-    fn setup_default_config(file: &Path) -> Result<Self, ConfigError> {
+    fn setup_default_config(config_file_path: &Path) -> Result<Self, ConfigError> {
         let note_directory = Self::get_document_directory()?.join("notes");
 
-        let parent = file.parent().ok_or_else(|| {
-            ConfigError::new(format!("Config path has no parent: {}", file.display()))
-        })?;
+        let config_directory = config_file_path
+            .parent()
+            .ok_or(ConfigError::DirectoryNotFound)?;
 
-        fs::create_dir_all(parent)
-            .map_err(|e| ConfigError::new(format!("Unable to create config directory: {e}")))?;
+        fs::create_dir_all(config_directory)
+            .map_err(|e| ConfigError::CreationError(e.to_string()))?;
+
+        fs::create_dir_all(&note_directory)
+            .map_err(|e| ConfigError::CreationError(e.to_string()))?;
 
         let default_config = AppConfig {
             notes_path: note_directory,
@@ -107,19 +106,9 @@ impl AppConfig {
         };
 
         let yaml = serde_yml::to_string(&default_config)
-            .map_err(|e| ConfigError::new(format!("Unable to serialize config: {e}")))?;
+            .map_err(|e| ConfigError::ParseError(e.to_string()))?;
 
-        fs::write(file, yaml).map_err(|e| {
-            ConfigError::new(format!(
-                "Unable to write config file {}: {e}",
-                file.display()
-            ))
-        })?;
-
-        log!("DEBUG", format!("Config file location: {}", file.display()));
-
-        fs::create_dir_all(&default_config.notes_path)
-            .map_err(|e| ConfigError::new(format!("Unable to create notes directory: {e}")))?;
+        fs::write(config_file_path, yaml).map_err(|e| ConfigError::WriteError(e.to_string()))?;
 
         Ok(default_config)
     }
